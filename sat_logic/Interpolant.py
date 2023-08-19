4d04d47c277bcf04e81776156fdc3e17137c3f3f
IlijaVorontsov <32547475+IlijaVorontsov@users.noreply.github.com>
2023-08-19 10:40:58 +0200

    fixes

--- sat_logic/Interpolant.py
+++ sat_logic/Interpolant.py
@@ -2,19 +2,20 @@ from sat_logic.ColoredLogic import ColorfulCNF
 from sat_logic.Logic import CNF, Clause
 from sat_logic.Solvers import ProofSolver
 
+
 class LabeledClause:
-    def __init__(self, clause: Clause, label:CNF, index:int) -> None:
+    def __init__(self, clause: Clause, label: CNF, index: int) -> None:
         self.clause = clause
         self.label = label
         self.index = index
         self.parents = []
-    
+
     def derivesEmpty(self) -> bool:
         return self.clause.isUnsat
-    
+
     def __hash__(self) -> int:
         return hash(self.index)
-    
+
     def __eq__(self, other: object) -> bool:
         if isinstance(other, int):
             return self.index == other
@@ -28,14 +29,41 @@ class ProofClause(LabeledClause):
         self.parents = [int(lit) for lit in parts[parts.index("0")+1:-1]]
         self.clause = Clause({int(lit) for lit in parts[1:parts.index("0")]})
         self.label = None
-        
+
+
+class LabeledClause:
+    def __init__(self, clause: Clause, label: CNF, index: int) -> None:
+        self.clause = clause
+        self.label = label
+        self.index = index
+        self.parents = []
+
+    def derivesEmpty(self) -> bool:
+        return self.clause.isUnsat
+
+    def __hash__(self) -> int:
+        return hash(self.index)
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, int):
+            return self.index == other
+        return self.index == other.index
+
+
+class ProofClause(LabeledClause):
+    def __init__(self, line: str) -> None:
+        parts = line.split()
+        self.index = int(parts[0])
+        self.parents = [int(lit) for lit in parts[parts.index("0")+1:-1]]
+        self.clause = Clause({int(lit) for lit in parts[1:parts.index("0")]})
+        self.label = None
+
 
 class Interpolant:
-    def __init__(self, prooffile:str, colorful_cnf:ColorfulCNF, assumptions=[]) -> None:
+    def __init__(self, prooffile: str, colorful_cnf: ColorfulCNF, assumptions=[]) -> None:
         self.colorful_cnf = colorful_cnf
         self.cnf_clauses = list(colorful_cnf)
-        self.cnf_clauses.insert(0, Clause()) # Shift the clauses by 1
-        self.cnf_clauses.insert(1, Clause({1})) # Add the constant true symbol
+        self.cnf_clauses.insert(0, Clause())  # Shift the clauses by 1
         self.color_variables = colorful_cnf.color[1].variables
 
         self.solver = ProofSolver()
@@ -53,23 +81,24 @@ class Interpolant:
     @property
     def cnf(self):
         return self.getLabeledClause(self.last_index).label
-    
+
     def getLabeledClause(self, index):
-        if index not in self.proof: # Root clause
+        if index not in self.proof:  # Root clause
             clause = self.cnf_clauses[index]
             label = None
             if clause in self.colorful_cnf.color[1]:
                 label = CNF([Clause({1})], keep_minimal=True)
             else:
-                label = CNF({clause.intersection(self.colorful_cnf.color[1].variables)}, keep_minimal=True)
+                label = CNF({clause.intersection(
+                    self.colorful_cnf.color[1].variables)}, keep_minimal=True)
             self.proof[index] = LabeledClause(clause, label, index)
             return self.proof[index]
-        
+
         current_labeled_clause = self.proof[index]
         label = current_labeled_clause.label
         if label:
             return current_labeled_clause
-        
+
         parents = current_labeled_clause.parents
 
         labeled_clause = self.getLabeledClause(parents[-1])
@@ -80,11 +109,10 @@ class Interpolant:
             labeled_clause = self.getLabeledClause(parents[i])
             clause = labeled_clause.clause
             resolvant = derived_clause.resolvant(clause)
-            print(f"resolving {derived_clause} with {clause} on {resolvant}")
             derived_clause = derived_clause.resolve_on(clause, resolvant)
             if resolvant not in self.color_variables:
                 label = label | labeled_clause.label
-            else: 
+            else:
                 label = label & labeled_clause.label
         labeled_clause = LabeledClause(derived_clause, label, index)
         self.proof[index] = labeled_clause
@@ -92,10 +120,9 @@ class Interpolant:
 
 
 if __name__ == "__main__":
-    from Logic import Clause
     # Test case taken from the book with a = 2, b = 3, ...
-    ccnf = ColorfulCNF([CNF([[-2, 5], [-2, 3, -5], [-2, -3], [2, -3], [2, 3, 5]]), CNF([[3, -5]])])
-    print(list(ccnf))
+    ccnf = ColorfulCNF(
+        [CNF([[-2, 5], [-2, 3, -5], [-2, -3], [2, -3], [2, 3, 5]]), CNF([[3, -5]])])
     interpolant = Interpolant("proof.lrat", ccnf)
     clauses = interpolant.cnf
     if Clause({-3}) in clauses and Clause({5}) in clauses and len(clauses) == 2:
